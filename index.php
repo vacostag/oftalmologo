de003268f533bfb2fa7a5b6f94a7fa33b485f1ef
Victor Acosta <vacosta@televen.vom>
2014-07-08 18:29:31 -0430

    home
    home is almost ready

---
+++ index.php
@@ -0,0 +1,128 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dr. J.J. Olivares | Cirugía plástica</title>
+		<link rel="stylesheet/less" type="text/css" href="css/style.less" />
+	</head>
+	<body>
+		<div class="wrapper">
+			<header>
+				<div class="logo">
+					<img src="assets/logo.jpg" title="Dr. J.J. Olivares" alt="Dr. J.J. Olivares"/>
+				</div>
+				<div class="social-networks">
+					<ul>
+						<li><img src="assets/networks/logo-tw.png" title="@" alt="@"/></li>
+						<li><img src="assets/networks/logo-fb.png" title="@" alt="@"/></li>
+						<li><img src="assets/networks/logo-ig.png" title="@" alt="@"/></li>
+						<li><img src="assets/networks/logo-yt.png" title="@" alt="@"/></li>
+						<li><img src="assets/networks/logo-feed.png" title="@" alt="@"/></li>
+					</ul>
+				</div>
+				<div class="payment-info">
+					<ul>
+						<li><img src="assets/logo-visa.png" title="Visa" alt="Visa"/></li>
+						<li><img src="assets/logo-master.png" title="Master" alt="Master"/></li>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+			</header>
+			<nav>
+				<ul>
+					<li>Conóceme</li>
+					<li>Procedimientos</li>
+					<li>Galería</li>
+					<li>Contáctame</li>
+				</ul>
+			</nav>
+			<section id="hero-shoot">
+				<div class="navigation">
+					
+				</div>
+				<div class="hero-images">
+					<ul>
+						<li><img src="assets/hero/1.jpg" /></li>
+					</ul>
+				</div>
+			</section>
+			<section id="procedures">
+				<ul>
+					<li>
+						<div class="p-title">Mamas</div>
+						<div class="p-image"><img src="assets/procedures/mamas.jpg" /></div>
+					</li>
+					<li>
+						<div class="p-title">Rinoplastia</div>
+						<div class="p-image"><img src="assets/procedures/rinoplastia.jpg" /></div>
+					</li>
+					<li>
+						<div class="p-title">Lipoescultura</div>
+						<div class="p-image"><img src="assets/procedures/lipoescultura.jpg" /></div>
+					</li>
+					<li>
+						<div class="p-title">Ritidectomía</div>
+						<div class="p-image"><img src="assets/procedures/ritidectomia.jpg" /></div>
+					</li>
+					<li>
+						<div class="p-title">Abdominoplastia</div>
+						<div class="p-image"><img src="assets/procedures/abdominoplastia.jpg" /></div>
+					</li>
+					<li>
+						<div class="p-title">Otros</div>
+						<div class="p-image"><img src="assets/procedures/otros.jpg" /></div>
+					</li>
+				</ul>
+			</section>
+			<section id="information">
+				<div class="bar-first">
+					<div class="left">
+						<p class="information-title">La cirugía plástica</p>
+						<p><img src="assets/blog/1.jpg" title="" alt="" width="335"/></p>
+					</div>
+					<div class="right">
+						<p class="information-title">¿Cómo escoger a un cirujano plástico?</p>
+						<p><img src="assets/blog/2.jpg" title="" alt=""  width="335"/></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="bar-second">
+					<div class="left">
+						<p class="information-title">La cirugía plástica</p>
+						<p><img src="assets/blog/3.jpg" title="" alt=""  width="335"/></p>
+					</div>
+					<div class="right">
+						<p class="information-title">¿Cómo escoger a un cirujano plástico?</p>
+						<p><img src="assets/blog/4.jpg" title="" alt=""  width="335"/></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="blog-info">
+					<img src="assets/banner-blog.jpg" title="blog" />
+				</div>
+			</section>
+			<aside>
+				<div class="twitter-feed-wrapper"><a class="twitter-timeline" width="240" height="263" href="https://twitter.com/TelevenTV" data-widget-id="410442845456048128">Tweets by @TelevenTV</a></div>
+				<div class="facebook-feed-wrapper"><div class="fb-activity" data-app-id="3b067fa997ec6f2ecaa695d154ca6e13" data-site="www.televen.com" data-action="likes, recommends, " data-width="240" data-height="272" data-colorscheme="light" data-header="true"></div></div>
+			</aside>
+			<div class="clearfix"></div>
+			<footer>
+				<div class="belongs-to"><img src="assets/belongs-to.jpg" title="Dr. J.J. Olivares pertenece a" /></div>
+				<div class="footer-text">
+					<p>Chuao, Caracas, Venezuela. CCCT Torre B, piso 12, consultorio Nro. 12-01</p>
+					<p>Todos los derechos reservados &copy; Copyright 2001 - <?php  echo date("Y");  ?> Dr. J.J. Olivares - RIF: J-5689752-3</p>
+				</div>
+			</footer>
+		</div>
+	</body>
+	<script src="js/less.js" type="text/javascript"></script>
+	<script src="http://www.google.com/jsapi"></script>
+	<script>google.load("jquery", "1");</script>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=493814407406301";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+</html>
